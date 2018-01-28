5de2b35a0959d25e423517a92963937bedf7b86d
dorz <dorz.ram@gmail.com>
2018-01-28 13:00:54 -0300

    Creating component for notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CommonComponent } from './common/common.component';
 
+import { Routes, RouterModule} from '@angular/router';
+
+
+const appRoutes: Routes = [
+	{ path: 'common', component: CommonComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, CommonComponent]
 })
 export class AppModule { }
